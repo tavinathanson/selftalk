0d07bfb53e3fe1d986239814279c1b4bfad2270e
tavi <tavi.nathanson@gmail.com>
2013-10-13 11:32:49 -0400

    Currently, the submit button submits the text of the input field to the DB, and displays it. Sweet.

--- selftalk.js
+++ selftalk.js
@@ -1,29 +1,19 @@
 Lines = new Meteor.Collection("lines");
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to selftalk.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    },
-		'keypress input' : function() {
-			// TODO	
+	Template.line_input.events({
+		'submit' : function() {
+    	Lines.insert({text: line.value});
 		}
-  });
+	});
+
 	Template.line_list.lines = function () {
 		return Lines.find({});
 	};
 }
 
 if (Meteor.isServer) {
-	Lines.insert({text: "test_line"});
-
   Meteor.startup(function () {
-    // code to run on server at startup
+    // No startup code, currently.
   });
 }
